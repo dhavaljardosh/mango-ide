74c11ed6fd50f08d52723909d6df87575f155ce0
Dhaval Jardosh <dhavaljardosh@gmail.com>
2026-04-07 04:17:24 -0500

    UI upload

--- worker/src/utils/cors.ts
+++ worker/src/utils/cors.ts
@@ -1,6 +1,7 @@
 export function corsHeaders() {
   return {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin":
+      "https://ide-worker.dhavaljardosh.workers.dev",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
   };
